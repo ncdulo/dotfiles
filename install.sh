c87c45d177b40ae280085b962dc9700256d3d6ee
ncdulo <nick.cardullo@gmail.com>
2019-12-10 20:03:17 -0500

    Added install script

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# Source: http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
+############################
+
+########## Variables
+
+dir=~/dev/dotfiles                    # dotfiles directory
+olddir=~/dev/dotfiles_old             # old dotfiles backup directory
+
+# list of files/folders to symlink in homedir
+files="bashrc vimrc zshrc"    
+
+##########
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file ~/dotfiles_old/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
+
