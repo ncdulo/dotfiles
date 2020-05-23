fbeb045a2e6ec151e5e8226916badcf0e7344506
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-05-23 11:46:46 -0400

    Include autorun script for AwesomeWM
    This script handles some startup programs for AwesomeWM which will be
    launched at session start. Some of this is to set up the environment for
    use by Awesome (font path, color profile, etc). Others are used for
    launching certain programs I wish to load automatically at each new
    session.
    
    The script is designed such that the auto-start programs which stay
    running in the background, or as tray icons, will only be executed if
    there is not already a running instance.
    
    The reference for this implementation was taken from the ArchWiki:
    https://wiki.archlinux.org/index.php/Awesome#Autostart

---
+++ .config/awesome/autorun.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Run some programs when Awesome starts.
+# If a program is intended to only have a single instance, call it using
+#   run program_name [arguments]
+# That is useful when re-loading Awesome so that we don't end up with
+# multiple instances of auto-start programs all over the place.
+
+function run {
+    if ! pgrep -f $1 ;
+    then
+        $@&
+    fi
+}
+
+# Setup programs, these fire off once and that's it
+xrandr --output DVI-I-1 --mode 1920x1080 --refresh 100
+xcalib -d :0 /home/ncdulo/ASUS_VG248QE_Nvidia.icm
+xset fp+ /usr/share/fonts/terminus
+xset fp+ /usr/share/fonts/inconsolata
+xset fp rehash
+numlockx on
+
+# Long running programs, should only run once
+run parcellite
+run pasystray
+run xscreensaver -nosplash
+run /home/ncdulo/dev/bin/process_xscreensaver.sh
