33359e0e014022c81e0c0203d81f245d586de2cd
ncdulo <957976+ncdulo@users.noreply.github.com>
2020-06-13 16:00:42 -0400

    Enable parinfer, dired icons
    Parinfer mode has been handling *very* nicely as a mostly automated
    means to prevent insanity from parentheses in Lisp editing modes. Very
    nice implementation, and so far pain free.
    
    Enable `all-the-icons` for Dired mode. Just a little eye-candy. Not sure
    I want to keep it, but it is helpful at least for now.

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -58,13 +58,13 @@
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
-       ;;parinfer          ; turn lisp into python, sort of
+       parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
        snippets          ; my elves. They type so I don't have to
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       dired             ; making dired pretty [functional]
+       (dired +icons)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
